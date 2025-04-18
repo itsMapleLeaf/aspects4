d97a0f256494a0cc4a7ceac46da3643b6841ea32
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-04-18 17:33:27 -0500

    fix user name not saving

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,7 +1,49 @@
 import { Anonymous } from "@convex-dev/auth/providers/Anonymous"
 import { Password } from "@convex-dev/auth/providers/Password"
 import { convexAuth } from "@convex-dev/auth/server"
+import { DataModel } from "./_generated/dataModel"
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-	providers: [Password, Anonymous],
+	providers: [
+		Password({
+			profile(params) {
+				console.log(params)
+
+				if (typeof params.email !== "string") {
+					throw new Error("Email is required")
+				}
+
+				if (params.flow === "signUp") {
+					if (typeof params.name !== "string") {
+						throw new Error("Name is required")
+					}
+					return {
+						name: params.name,
+						email: params.email,
+					}
+				}
+
+				if (params.flow === "signIn") {
+					return {
+						// typescript made me do this
+						...(typeof params.name === "string" && { name: params.name }),
+						email: params.email,
+					}
+				}
+
+				throw new Error(`Unknown auth flow "${params.flow}"`)
+			},
+		}),
+		Anonymous<DataModel>({
+			profile(params) {
+				if (typeof params.name !== "string") {
+					throw new Error("Name is required")
+				}
+				return {
+					name: params.name,
+					isAnonymous: true,
+				}
+			},
+		}),
+	],
 })
