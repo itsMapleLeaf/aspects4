ccd778c5afcae53dd142e3a8cf233d82f984702e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-04-22 23:42:53 -0500

    update schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,33 +1,42 @@
 import { authTables } from "@convex-dev/auth/server"
 import { defineSchema, defineTable } from "convex/server"
-import { v } from "convex/values"
+import { v, type Validator } from "convex/values"
 
 export default defineSchema({
 	...authTables,
 
+	characters: defineTable({
+		ownerId: v.id("users"),
+		roomId: optionull(v.id("rooms")),
+		key: v.string(),
+		clientData: v.optional(v.any()),
+	})
+		.index("roomId", ["roomId", "clientData.name"])
+		.index("ownerId", ["ownerId", "clientData.name"])
+		.index("key", ["key", "roomId", "clientData.name"]),
+
 	rooms: defineTable({
 		name: v.string(),
 		slug: v.string(),
-		backgroundId: v.optional(v.union(v.id("_storage"), v.null())),
+		backgroundId: optionull(v.id("_storage")),
 		ownerId: v.optional(v.id("users")),
 		memberUserIds: v.optional(v.array(v.id("users"))),
 	})
 		.index("name", ["name"])
 		.index("slug", ["slug"]),
 
+	roomCharacters: defineTable({
+		roomId: v.id("rooms"),
+		characterId: v.id("characters"),
+	})
+		.index("roomId", ["roomId"])
+		.index("characterId", ["characterId"]),
+
 	messages: defineTable({
 		sender: v.string(),
 		content: v.string(),
 		roomId: v.id("rooms"),
 	}).index("roomId", ["roomId"]),
-
-	characters: defineTable({
-		roomId: v.id("rooms"),
-		key: v.string(),
-		clientData: v.optional(v.any()),
-	})
-		.index("roomId", ["roomId", "clientData.name"])
-		.index("key", ["key", "roomId", "clientData.name"]),
 
 	assets: defineTable({
 		name: v.string(),
@@ -49,3 +58,9 @@ export default defineSchema({
 		updatedAt: v.number(),
 	}).index("roomId", ["roomId"]),
 })
+
+function optionull<T, FieldPaths extends string>(
+	validator: Validator<T, "required", FieldPaths>,
+) {
+	return v.optional(v.union(validator, v.null()))
+}
