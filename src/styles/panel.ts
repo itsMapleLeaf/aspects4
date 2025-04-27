a05bdf4f370b8649bf5a85470187f9d324aaa394
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-04-27 01:47:49 -0500

    deprecate panel

--- src/styles/panel.ts
+++ src/styles/panel.ts
@@ -1,5 +1,6 @@
 import { ClassNameValue, twMerge } from "tailwind-merge"
 
+/** @deprecated Use `panel` from `@/styles/panel.css` instead. */
 export const panel = (...classes: ClassNameValue[]) =>
 	twMerge(
 		"rounded-md border overflow-clip border-gray-800 bg-gray-900 p-3 shadow-md shadow-black/50",
