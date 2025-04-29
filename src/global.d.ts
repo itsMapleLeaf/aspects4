17c3d68ad7309aa506b08b9a0ebc047c5fc72582
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-04-28 19:41:53 -0500

    add useActionState overload for no initial state arg

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,2 +1,13 @@
 /// <reference types="vite/client" />
 /// <reference types="@total-typescript/ts-reset" />
+
+import "react"
+declare module "react" {
+	export function useActionState<Input, Result>(
+		action: (state: Awaited<Result> | undefined, input: Input) => Result,
+	): [
+		state: Awaited<Result> | undefined,
+		dispatch: (input: Input) => void,
+		isPending: boolean,
+	]
+}
