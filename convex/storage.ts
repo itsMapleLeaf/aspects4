b8ff48ab64d98e19a4ea59a923c8c0b31698c872
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-05-08 17:59:08 -0500

    might be useful later

--- convex/storage.ts
+++ convex/storage.ts
@@ -1,8 +1,16 @@
-import { mutation } from "./_generated/server"
+import { v } from "convex/values"
+import { internalQuery, mutation } from "./_generated/server"
 
 export const generateUploadUrl = mutation({
 	args: {},
 	async handler(ctx) {
 		return await ctx.storage.generateUploadUrl()
+	},
+})
+
+export const getMetadata = internalQuery({
+	args: { storageId: v.id("_storage") },
+	async handler(ctx, args) {
+		return await ctx.db.system.get(args.storageId)
 	},
 })
