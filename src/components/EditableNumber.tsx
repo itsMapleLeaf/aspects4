987e60c3c8e3bcd99fdb3099912626d5c36a801c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-05-18 02:32:34 -0500

    improve EditableNumber
    refactor + allow scrolling without having to click first

--- src/components/EditableNumber.tsx
+++ src/components/EditableNumber.tsx
@@ -1,6 +1,7 @@
 import { clamp } from "es-toolkit"
-import { useState } from "react"
+import { useId, useState, type ComponentProps, type ReactNode } from "react"
 import { Button } from "~/components/ui/Button.tsx"
+import { Field } from "~/components/ui/Field.tsx"
 import { Input } from "~/components/ui/Input.tsx"
 import { parseNumberSafe } from "~/lib/utils.ts"
 
@@ -20,7 +21,7 @@ export function EditableNumber({
 	onChange: (value: number) => void
 }) {
 	const value = clamp(valueProp, min, max)
-	const [editing, setEditing] = useState(false)
+	const [editingValue, setEditingValue] = useState<string>()
 
 	const wheelHandlerRef = (element: HTMLElement | null) => {
 		const controller = new AbortController()
@@ -28,25 +29,18 @@ export function EditableNumber({
 		element?.addEventListener(
 			"wheel",
 			(event) => {
-				const tweak = (delta: number) => {
-					event.preventDefault()
-
-					const currentValue =
-						event.target instanceof HTMLInputElement ?
-							(parseNumberSafe(event.target.value) ?? 0)
-						:	value
-
-					const newValue = clamp(currentValue + delta, min, max)
-
-					if (event.target instanceof HTMLInputElement) {
-						event.target.value = String(newValue)
-					}
-
-					onChange(newValue)
-				}
-
 				if (event.deltaY < 0) tweak(1)
 				if (event.deltaY > 0) tweak(-1)
+
+				function tweak(delta: number) {
+					event.preventDefault()
+
+					const currentValue = parseNumberSafe(editingValue) ?? value
+					const newValue = clamp(currentValue + delta, min, max)
+
+					onChange(newValue)
+					setEditingValue(String(newValue))
+				}
 			},
 			{ signal: controller.signal, passive: false },
 		)
@@ -54,56 +48,76 @@ export function EditableNumber({
 		return () => controller.abort()
 	}
 
-	return editing ?
-			<Input
-				id={id}
-				className={className}
-				align="center"
-				inputMode="numeric"
-				autoFocus
-				defaultValue={value}
-				ref={wheelHandlerRef}
-				onFocus={(event) => {
-					event.currentTarget.select()
-				}}
-				onBlur={(event) => {
-					onChange(clamp(Number(event.currentTarget.value) || 0, min, max))
-					setEditing(false)
-				}}
-				onKeyDown={(event) => {
-					if (event.key === "Enter") {
-						event.preventDefault()
-						onChange(clamp(Number(event.currentTarget.value) || 0, min, max))
-						setEditing(false)
-					}
-					if (event.key === "ArrowUp") {
-						event.preventDefault()
-						const newValue = clamp(
-							(Number(event.currentTarget.value) || 0) + 1,
+	return (
+		<div ref={wheelHandlerRef} className="contents">
+			{editingValue != null ?
+				<Input
+					id={id}
+					className={className}
+					align="center"
+					inputMode="numeric"
+					value={editingValue}
+					autoFocus
+					onFocus={(event) => {
+						event.currentTarget.select()
+					}}
+					onChange={(event) => {
+						setEditingValue(event.currentTarget.value)
+					}}
+					onBlur={() => {
+						onChange(clamp(parseNumberSafe(editingValue) ?? 0, min, max))
+						setEditingValue(undefined)
+					}}
+					onKeyDown={(event) => {
+						const normalizedEditingValue = clamp(
+							parseNumberSafe(editingValue) ?? 0,
 							min,
 							max,
 						)
-						event.currentTarget.value = String(newValue)
-						onChange(newValue)
-					}
-					if (event.key === "ArrowDown") {
-						event.preventDefault()
-						const newValue = clamp(
-							(Number(event.currentTarget.value) || 0) - 1,
-							min,
-							max,
-						)
-						event.currentTarget.value = String(newValue)
-						onChange(newValue)
-					}
-				}}
-			/>
-		:	<Button
-				onClick={() => setEditing(true)}
-				onFocus={() => setEditing(true)}
-				align="center"
-				className={className}
-			>
-				{value}
-			</Button>
+
+						if (event.key === "Enter") {
+							event.preventDefault()
+							onChange(normalizedEditingValue)
+							setEditingValue(undefined)
+						}
+
+						if (event.key === "ArrowUp") {
+							event.preventDefault()
+							const newValue = normalizedEditingValue + 1
+							setEditingValue(String(newValue))
+							onChange(newValue)
+						}
+
+						if (event.key === "ArrowDown") {
+							event.preventDefault()
+							const newValue = normalizedEditingValue - 1
+							setEditingValue(String(newValue))
+							onChange(newValue)
+						}
+					}}
+				/>
+			:	<Button
+					id={id}
+					onClick={() => setEditingValue(String(value))}
+					onFocus={() => setEditingValue(String(value))}
+					align="center"
+					className={className}
+				>
+					{value}
+				</Button>
+			}
+		</div>
+	)
+}
+
+export function EditableNumberField({
+	label,
+	...props
+}: ComponentProps<typeof EditableNumber> & { label: ReactNode }) {
+	const id = useId()
+	return (
+		<Field label={label} htmlFor={props.id ?? id}>
+			<EditableNumber id={id} {...props} />
+		</Field>
+	)
 }
