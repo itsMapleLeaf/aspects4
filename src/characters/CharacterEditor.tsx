2e5eaa7c0f2f83fdfe4abdf17778b46bfe6ac4cd
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-05-28 01:06:37 -0500

    split out some options

--- src/characters/CharacterEditor.tsx
+++ src/characters/CharacterEditor.tsx
@@ -6,6 +6,7 @@ import { toTitleCase } from "~/lib/utils.ts"
 import { EditableTextField } from "../components/EditableTextField.tsx"
 import listOfLineages from "../data/list-of-lineages.json"
 import type { Character } from "./character.ts"
+import { auraOptions, budgetOptions, itemTypeOptions } from "./data.ts"
 import {
 	SheetNumberField,
 	SheetSelectField,
@@ -55,91 +56,6 @@ export function CharacterEditor({
 		attributeFields.sense.value +
 		attributeFields.intellect.value +
 		attributeFields.presence.value
-
-	const budgetOptions = [
-		{
-			value: "dirt",
-			label: "1. Dirt",
-			description: "Water and other freely-available resources",
-		},
-		{
-			value: "cheap",
-			label: "2. Cheap",
-			description: "Common meals, simple clothing",
-		},
-		{
-			value: "inexpensive",
-			label: "3. Inexpensive",
-			description:
-				"Five-star meals, basic tools and weapons, reasonable lodging",
-		},
-		{
-			value: "steep",
-			label: "4. Steep",
-			description:
-				"Premium tools and weapons, extravagant clothing, comfortable lodging",
-		},
-		{
-			value: "expensive",
-			label: "5. Expensive",
-			description: "A house, luxurious lodging",
-		},
-		{ value: "valuable", label: "6. Valuable", description: "A mansion" },
-		{
-			value: "priceless",
-			label: "7. Priceless",
-			description: "An extremely rare, precious, powerful artifact",
-		},
-	]
-
-	const itemTypeOptions = [
-		{
-			value: "consumable",
-			label: "Consumable",
-			description: "Goes away when there are no more uses",
-		},
-		{
-			value: "tool",
-			label: "Tool",
-			description: "Can be used and reused while held",
-		},
-		{
-			value: "wearable",
-			label: "Wearable",
-			description: "Can be worn for a persistent effect",
-		},
-	]
-
-	const auraOptions = [
-		{
-			value: "Fire",
-			label: "Fire",
-			description:
-				"Indicates an adversarial, heated, conflict-heavy relationship.",
-		},
-		{
-			value: "Water",
-			label: "Water",
-			description: "Comes from notions of comfort, peace, and protection.",
-		},
-		{
-			value: "Wind",
-			label: "Wind",
-			description:
-				"Exhibits in turbulent relationships full of excitement and change.",
-		},
-		{
-			value: "Light",
-			label: "Light",
-			description:
-				"Represents diplomatic relationships built on fairness and respect.",
-		},
-		{
-			value: "Darkness",
-			label: "Darkness",
-			description: "Manifests from tension, mistrust, and uncertainty.",
-		},
-	]
 
 	return (
 		<div className="grid gap-8">
---
+++ src/characters/data.ts
@@ -0,0 +1,81 @@
+export const budgetOptions = [
+	{
+		value: "dirt",
+		label: "1. Dirt",
+		description: "Water and other freely-available resources",
+	},
+	{
+		value: "cheap",
+		label: "2. Cheap",
+		description: "Common meals, simple clothing",
+	},
+	{
+		value: "inexpensive",
+		label: "3. Inexpensive",
+		description: "Five-star meals, basic tools and weapons, reasonable lodging",
+	},
+	{
+		value: "steep",
+		label: "4. Steep",
+		description:
+			"Premium tools and weapons, extravagant clothing, comfortable lodging",
+	},
+	{
+		value: "expensive",
+		label: "5. Expensive",
+		description: "A house, luxurious lodging",
+	},
+	{ value: "valuable", label: "6. Valuable", description: "A mansion" },
+	{
+		value: "priceless",
+		label: "7. Priceless",
+		description: "An extremely rare, precious, powerful artifact",
+	},
+]
+export const itemTypeOptions = [
+	{
+		value: "consumable",
+		label: "Consumable",
+		description: "Goes away when there are no more uses",
+	},
+	{
+		value: "tool",
+		label: "Tool",
+		description: "Can be used and reused while held",
+	},
+	{
+		value: "wearable",
+		label: "Wearable",
+		description: "Can be worn for a persistent effect",
+	},
+]
+export const auraOptions = [
+	{
+		value: "Fire",
+		label: "Fire",
+		description:
+			"Indicates an adversarial, heated, conflict-heavy relationship.",
+	},
+	{
+		value: "Water",
+		label: "Water",
+		description: "Comes from notions of comfort, peace, and protection.",
+	},
+	{
+		value: "Wind",
+		label: "Wind",
+		description:
+			"Exhibits in turbulent relationships full of excitement and change.",
+	},
+	{
+		value: "Light",
+		label: "Light",
+		description:
+			"Represents diplomatic relationships built on fairness and respect.",
+	},
+	{
+		value: "Darkness",
+		label: "Darkness",
+		description: "Manifests from tension, mistrust, and uncertainty.",
+	},
+]
