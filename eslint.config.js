00067ce2ea5697ad0f15b3a6f8e6485f22ea44f3
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-06-19 00:44:11 -0500

    add react rules (mainly for the missing key rule)

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,18 @@ import js from "@eslint/js"
 import reactCompiler from "eslint-plugin-react-compiler"
 import reactHooks from "eslint-plugin-react-hooks"
 import reactRefresh from "eslint-plugin-react-refresh"
+import react from "eslint-plugin-react-x"
 import globals from "globals"
 import tseslint from "typescript-eslint"
 
 export default tseslint.config(
 	{ ignores: ["dist", "convex/_generated"] },
 	{
-		extends: [js.configs.recommended, ...tseslint.configs.recommended],
+		extends: [
+			js.configs.recommended,
+			...tseslint.configs.recommended,
+			react.configs.recommended,
+		],
 		files: ["**/*.{ts,tsx}"],
 		languageOptions: {
 			ecmaVersion: 2024,
@@ -33,6 +38,7 @@ export default tseslint.config(
 				{ allowConstantExport: true },
 			],
 			"react-compiler/react-compiler": "warn",
+			"react-x/no-clone-element": "off",
 		},
 	},
 )
