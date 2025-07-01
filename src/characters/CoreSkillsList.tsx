021995f5a07b177bad4f824d2e440ea6e79227c1
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2025-07-01 03:48:21 -0500

    calculate peril debuffs

--- src/characters/CoreSkillsList.tsx
+++ src/characters/CoreSkillsList.tsx
@@ -1,61 +1,76 @@
 import { Heading, HeadingLevel } from "@ariakit/react"
+import { sum } from "es-toolkit"
 import { resolveMilestoneListFieldItems } from "~/characters/milestones.ts"
 import { useEditorCharacterSheet } from "./context.tsx"
 import { SKILLS } from "./data.ts"
 import { resolveNumberField } from "./sheet/fields.ts"
 import { SheetStatField } from "./sheet/SheetStatField.tsx"
+import { resolveStress } from "./stress.ts"
 
 export function CoreSkillsList() {
 	const sheet = useEditorCharacterSheet()
-
+	const stress = resolveStress(sheet)
 	const milestones = resolveMilestoneListFieldItems(sheet)
 
 	return (
 		<div className="@container grid gap-4">
-			{Object.entries(SKILLS).map(([category, skills]) => (
-				<HeadingLevel key={category}>
-					<section>
-						<Heading className="mb-1 heading-xl">{category}</Heading>
-						<ul className="grid grid-cols-2 gap-2">
-							{Object.entries(skills)
-								.sort(([a], [b]) =>
-									a.toLowerCase().localeCompare(b.toLowerCase()),
-								)
-								.map(([skillName, skill]) => {
-									const field = resolveNumberField(sheet, {
-										id: `coreSkills:${skillName}`,
-										min: 0,
-									})
-									const milestoneBonusCount = milestones.filter(
-										(it) => it.skillBonus.value === skillName,
-									).length
-									return (
-										<li key={skillName}>
-											<SheetStatField
-												label={skillName}
-												tooltip={skill.description}
-												resolved={field}
-												score={field.value + milestoneBonusCount}
-											/>
-										</li>
+			{Object.entries(SKILLS).map(([category, skills]) => {
+				const categoryPeril = sum(
+					stress.pools
+						.filter((it) => it.skillCategory === category)
+						.map((it) => it.peril),
+				)
+
+				return (
+					<HeadingLevel key={category}>
+						<section>
+							<div className="flex items-baseline gap-2">
+								<Heading className="mb-1 heading-xl">{category}</Heading>
+								{categoryPeril > 0 && (
+									<p className="muted">{`(-${categoryPeril} from peril)`}</p>
+								)}
+							</div>
+
+							<ul className="grid grid-cols-2 gap-2">
+								{Object.entries(skills)
+									.sort(([a], [b]) =>
+										a.toLowerCase().localeCompare(b.toLowerCase()),
 									)
-								})}
-						</ul>
-					</section>
-				</HeadingLevel>
-			))}
-			{/* <div className="grid gap-3 @sm:grid-cols-2">
-				{fields.map((field) => (
-					<SheetStatField
-						key={field.id}
-						resolved={field}
-						label={field.info.skill}
-						description={field.info.attribute}
-						score={field.value}
-						tooltip={field.info.flavor}
-					/>
-				))}
-			</div> */}
+									.map(([skillName, skill]) => {
+										const field = resolveNumberField(sheet, {
+											id: `coreSkills:${skillName}`,
+											min: 0,
+										})
+
+										const milestoneBonusCount = milestones.filter(
+											(it) => it.skillBonus.value === skillName,
+										).length
+
+										const score = Math.max(
+											0,
+											field.value + milestoneBonusCount - categoryPeril,
+										)
+
+										return (
+											<li key={skillName} className="grid">
+												<SheetStatField
+													label={skillName}
+													description={
+														milestoneBonusCount > 0 &&
+														`+${milestoneBonusCount} from milestones`
+													}
+													tooltip={skill.description}
+													resolved={field}
+													score={score}
+												/>
+											</li>
+										)
+									})}
+							</ul>
+						</section>
+					</HeadingLevel>
+				)
+			})}
 		</div>
 	)
 }
